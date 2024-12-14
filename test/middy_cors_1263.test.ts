36dc31fb466af35803a90736fd0dc90d39699223
Manuel <wtfjoke@livenet.ch>
2024-12-14 13:51:20 +0100

    Initial commit

---
+++ test/middy_cors_1263.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MiddyCors1263 from '../lib/middy_cors_1263-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/middy_cors_1263-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MiddyCors1263.MiddyCors1263Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
